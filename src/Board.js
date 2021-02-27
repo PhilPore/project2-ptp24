d8cdd82c4a135791d3167fb0ab0ffdec645fefd9
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-02-27 00:24:23 +0000

    Made a new file to hold my win check. As much as I am not a fan of having a lot of files, the check is pretty bulky and I like code being pretty. At least it's a O(1+C)

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from 'react';
 import io from 'socket.io-client';
 import { ShowSquare } from './Square.js'
+import { CheckEnd } from './EndCheck.js'
 import './Board.css'
 var turns = 0;
 const socket = io()
@@ -13,70 +14,6 @@ export function DispBoard () {
   const [stroke, setStroke] = useState(0) //logic is that every 3 clicks there is a probability of a winning move
   const [Win,setWin] = useState(0); //1 is a win, -1 is draw.
   
-  function CheckEnd(last_ind,marker){ //will use this for a win condition/draw condition
-    console.log("Entering check "+last_ind+"="+marker);
-    let fillcount = 0;
-    let i = 0;
-    if (last_ind == 0 || last_ind == 3 || last_ind == 6){
-      if (board[last_ind+1] == marker && board[last_ind+2] == marker){
-        console.log("We have a winner!");
-      }
-      if(last_ind == 0){
-        if ((board[last_ind+4] == marker && board[last_ind+8] == marker) || (board[3] == marker && board[6] == marker)){
-          console.log("We have a winner!");
-        }
-      }
-      else if (last_ind == 3){
-        if (board[0] == marker && board[6] == marker){
-          console.log("We have a winner!");
-        }
-      }
-      
-      else if ((board[3] == marker && board[0] == marker) || (board[4] == marker && board[2] == marker)){
-        console.log("We have a winner!");
-      }
-     
-    }
-    else if (last_ind == 1 || last_ind == 4 || last_ind == 7){
-      if (board[last_ind-1] == marker && board[last_ind+1] == marker){
-        console.log("We have a winner!")
-        }
-      else if (last_ind == 4){
-        if ((board[0] == marker && board[8] == marker) || (board[2] == marker && board[6] == marker)){
-          console.log("We have a winner!")
-        }
-      }
-    }
-    else if (last_ind == 2 || last_ind == 5 || last_ind == 8){
-      if (board[last_ind-1] == marker && board[last_ind-2] == marker){
-        console.log("We have a winner!");
-      }
-      else if (last_ind == 2){
-        if ((board[4] == marker && board[6] == marker) || (board[5]==marker && board[8] == marker )){
-          console.log("We have a winner");
-        }
-      }
-       else if (last_ind == 8){
-         if ((board[4] == marker && board[0] == marker) || (board[5]==marker && board[2] == marker )){
-          console.log("We have a winner");
-        }
-         
-       }
-      
-    }
-    
-  for(i=0; i<9;i++){
-      if (board[i] !=" " || i == last_ind){
-        fillcount+=1;
-      }
-    }
-    if (fillcount==9){
-      console.log("Draw!");
-      }      
-        
-  }
-  
-    
   
   
   
@@ -104,7 +41,7 @@ export function DispBoard () {
     
     //add function here to do checks
     //CheckEnd(value,marker);
-    CheckEnd(value,marker);
+    CheckEnd(value,marker,board);
     const ind_val = value;
     console.log("Emitting: "+ ind_val+" | "+board[ind_val]);
     socket.emit('cell',{pos:ind_val,bcell:marker,upd_tur:turn+1 });
---
+++ src/EndCheck.js
@@ -0,0 +1,63 @@
+export function CheckEnd(last_ind,marker,board){ //will use this for a win condition/draw condition
+    console.log("Entering check "+last_ind+"="+marker);
+    let fillcount = 0;
+    let i = 0;
+    if (last_ind == 0 || last_ind == 3 || last_ind == 6){
+      if (board[last_ind+1] == marker && board[last_ind+2] == marker){
+        console.log("We have a winner!");
+      }
+      if(last_ind == 0){
+        if ((board[last_ind+4] == marker && board[last_ind+8] == marker) || (board[3] == marker && board[6] == marker)){
+          console.log("We have a winner!");
+        }
+      }
+      else if (last_ind == 3){
+        if (board[0] == marker && board[6] == marker){
+          console.log("We have a winner!");
+        }
+      }
+      
+      else if ((board[3] == marker && board[0] == marker) || (board[4] == marker && board[2] == marker)){
+        console.log("We have a winner!");
+      }
+     
+    }
+    else if (last_ind == 1 || last_ind == 4 || last_ind == 7){
+      if (board[last_ind-1] == marker && board[last_ind+1] == marker){
+        console.log("We have a winner!")
+        }
+      else if (last_ind == 4){
+        if ((board[0] == marker && board[8] == marker) || (board[2] == marker && board[6] == marker)){
+          console.log("We have a winner!")
+        }
+      }
+    }
+    else if (last_ind == 2 || last_ind == 5 || last_ind == 8){
+      if (board[last_ind-1] == marker && board[last_ind-2] == marker){
+        console.log("We have a winner!");
+      }
+      else if (last_ind == 2){
+        if ((board[4] == marker && board[6] == marker) || (board[5]==marker && board[8] == marker )){
+          console.log("We have a winner");
+        }
+      }
+       else if (last_ind == 8){
+         if ((board[4] == marker && board[0] == marker) || (board[5]==marker && board[2] == marker )){
+          console.log("We have a winner");
+        }
+         
+       }
+      
+    }
+    
+  for(i=0; i<9;i++){
+      if (board[i] !=" " || i == last_ind){
+        fillcount+=1;
+      }
+    }
+    if (fillcount==9){
+      console.log("Draw!");
+      }      
+        
+  }
+  
