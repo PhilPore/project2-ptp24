f2c9dd652ac99e9e06cd763ff9b76e1b1849ba66
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-03-09 19:25:44 +0000

    First commit to Milestone3. What if I told you that beneath this mail of chain...

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+'''Server for the tic tac toe. Handles DB/Users, Turns, and Sockets'''
 import os
 from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
+import models
 
 
 load_dotenv(find_dotenv())
 
-user_types = [] #user list
+USER_TYPES = [] #user list
 
 app = Flask(__name__, static_folder='./build/static')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
@@ -18,7 +20,7 @@ db = SQLAlchemy(app)
 
 # IMPORTANT: This must be AFTER creating db variable to prevent
 # circular import issues
-import models
+#import models
 db.create_all()
 #models.Person.query.all()
 
@@ -46,11 +48,10 @@ def on_connect():
     leadlist_name = []
     leadlist_score = []
     for pers in leaderboard:
-        leadlist_name.append([pers.username,pers.score])
+        leadlist_name.append([pers.username, pers.score])
         #leadlist_score.append([pers.score])
     #print("{}\n{}".format(leadlist_name,leadlist_score))
-    socketio.emit('leaderboard',{"names":leadlist_name})
-    #leaderboard
+    socketio.emit('leaderboard', {"names":leadlist_name})
 
 # When a client disconnects from this Socket connection, this function is run
 @socketio.on('disconnect')
@@ -61,9 +62,9 @@ def on_disconnect():
 #Append to userlist
 def on_log(data):
     print(str(data))
-    if data['user'] not in user_types:
-        user_types.append(data['user'])
-    flag = 0 
+    if data['user'] not in USER_TYPES:
+        USER_TYPES.append(data['user'])
+    flag = 0
     quer = models.Person.query.filter_by(username=data['user']).first()
     if quer == None:
         flag = 1
@@ -71,12 +72,10 @@ def on_log(data):
         db.session.add(new_user)
         db.session.commit()
     leaderboard = db.session.query(models.Person).order_by(models.Person.score.desc())
-    leadlist_name = []
+    lst_name = []
     for pers in leaderboard:
-        leadlist_name.append([pers.username,pers.score])
-    
-   
-    socketio.emit('login', {'lst':user_types, 'flag':flag, 'names':leadlist_name}, broadcast=True, include_self=True)
+        lst_name.append([pers.username, pers.score])
+    socketio.emit('login', {'lst':USER_TYPES, 'flag':flag, 'names':lst_name}, broadcast=True, include_self=True)
 # When a client emits the event 'chat' to the server, this function is run
 # 'chat' is a custom event name that we just decided
 @socketio.on('cell')
@@ -84,41 +83,39 @@ def on_chat(data): # data is whatever arg you pass in your emit call on client
     print(str(data))
     # This emits the 'chat' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
-    socketio.emit('cell',  data, broadcast=True, include_self=False)
+    socketio.emit('cell', data, broadcast=True, include_self=False)
 
 @socketio.on('end')
 def game_over(data):
     print(str(data))
     if data["Cond"] == 1:
         win_ind = 0
-        loser_ind = 1
-        if data["player"] == user_types[0]:
+        lose_ind = 1
+        if data["player"] == USER_TYPES[0]:
             print("player X")
-        elif data["player"] == user_types[1]:
+        elif data["player"] == USER_TYPES[1]:
             print("Player O")
             win_ind = 1
-            loser_ind = 0
-        win_quer = db.session.query(models.Person).filter_by(username=user_types[win_ind]).first()
+            lose_ind = 0
+        win_quer = db.session.query(models.Person).filter_by(username=USER_TYPES[win_ind]).first()
         win_quer.score = win_quer.score+1
         db.session.commit()
-        lose_quer = db.session.query(models.Person).filter_by(username=user_types[loser_ind]).first()
+        lose_quer = db.session.query(models.Person).filter_by(username=USER_TYPES[lose_ind]).first()
         lose_quer.score = lose_quer.score-1
         db.session.commit()
         print(win_quer.score)
         #lose_quer.score = lose_quer.score-1
         print(lose_quer.score)
-        db.session.commit()
+        #db.session.commit()
         leaderboard = db.session.query(models.Person).order_by(models.Person.score.desc())
         leadlist_name = []
         for pers in leaderboard:
-            print("{} = {}".format(pers.username,pers.score))
-            leadlist_name.append([pers.username,pers.score])
+            print("{} = {}".format(pers.username, pers.score))
+            leadlist_name.append([pers.username, pers.score])
         socketio.emit('upd_l', {'names':leadlist_name}, broadcast=True, include_self=True)
-
-        
-    user_types.clear()
+    USER_TYPES.clear()
     print("Users cleared.")
-    print(user_types)
+    print(USER_TYPES)
 
 if __name__ == "__main__":
 
