a021320e40614922c663780b0b7a92d214bbe91a
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-03-11 18:37:56 +0000

    Updated bash file to check models.py. Also made it prettier. Under my helm there lies no face nor living brain. THIS EMPTINESS ENSURES THAT I WILL FEEL NOTHING.

--- models.py
+++ models.py
@@ -1,8 +1,14 @@
+'''Database info'''
 from app import db
 
+
 class Person(db.Model):
+    '''Data for the person database'''
     #id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False,primary_key=True)
+    username = db.Column(db.String(80),
+                         unique=True,
+                         nullable=False,
+                         primary_key=True)
     score = db.Column(db.Integer(), nullable=False)
 
     def __repr__(self):
