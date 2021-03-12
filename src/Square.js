1036184cd19f5d4e4635b22cc915d80e4221350c
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-03-12 06:15:52 +0000

    Eslint is cursed. Things are pretty at the very least but I don't think airbnb and I will jive well anytime soon. I see you moving with detect life. I KNOW YOU CANT FEEL ME. PRAYING TO POWERS OF THE NIGHT EYE. I STAND HERE FOR YOU TO SEE.

--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,13 @@
-import "./Board.css";
+import './Board.css';
+
 export function ShowSquare(props) {
-  console.log(props.val + "|" + props.cell);
   return (
-    <div class="box" onClick={() => props.Clickbox(props.val)}>
-      {" "}
-      {props.cell}{" "}
+    <div className="box" onClick={() => props.Clickbox(props.val)}>
+      {' '}
+      {props.cell}
+      {' '}
     </div>
   );
 }
+
+export default ShowSquare;
