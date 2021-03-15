84b15e4e1d81c9c79f77d7b389b9943426b84f8b
EC2 Default User <ec2-user@ip-172-31-39-30.ec2.internal>
2021-03-15 02:12:00 +0000

    Bash now has labels, as to not confuse anyone.

--- checks.sh
+++ checks.sh
@@ -1,2 +1,4 @@
+echo "app.py"
 pylint --rcfile=/home/ec2-user/environment/project2-ptp24/react-starter/standard.rc app.py
+echo "models.py"
 pylint --rcfile=/home/ec2-user/environment/project2-ptp24/react-starter/standard.rc models.py
